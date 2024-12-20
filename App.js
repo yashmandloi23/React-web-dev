88d62bd90c57d9b45a3e4711bee754c155c0a40f
yashmandloi23 <121743766+yashmandloi23@users.noreply.github.com>
2024-12-20 18:49:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Header} from './Header';
+import {Demo2} from './Header';
+function App() {
+  return (
+    <>
+    <Header/>
+    <Demo/>
+    <Demo2/>
+    </>
+  );
+}
+
+
+export default App;
+function Demo()
+{
+    return (
+    <h2>How are you ?</h2>
+    )
+}
+
---
+++ Header.js
@@ -0,0 +1,31 @@
+// import React from 'react'
+import './App.css';
+function Header() {
+  return (
+    <div className="menubar">
+			<div className="logo">
+			 <h2>LOGO</h2>
+			</div>
+			<div className="options">
+				<ul>	
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Home</a></li>
+				</ul>
+			</div>
+		</div>
+  )
+}
+
+export  { Header };
+
+function Demo2()
+{
+  return(
+    <div className="de">
+    <h1>I am Fine</h1>
+    </div>
+  )
+}
+export {Demo2};
